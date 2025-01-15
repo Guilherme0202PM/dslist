b30b57e6ae91b6ece76bd0451d9526b2ebc9d57c
Guilherme0202PM <46054442+Guilherme0202PM@users.noreply.github.com>
2025-01-15 14:21:21 -0300

    Envio 3.0
    Adicionando GameMinDTO

---
+++ src/main/java/com/dev/dslist/dto/GameMinDTO.java
@@ -0,0 +1,21 @@
+package com.dev.dslist.dto;
+
+import com.devr.dslist.entities.Game;
+
+public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO(Game entity) {
+        id = entity.getId(); 
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+}
