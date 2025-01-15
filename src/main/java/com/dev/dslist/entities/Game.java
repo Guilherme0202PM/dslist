ef458baf4b1ac6b2120f5ea74a84fadc6b1fccba
Guilherme0202PM <46054442+Guilherme0202PM@users.noreply.github.com>
2025-01-14 23:49:44 -0300

    Envio 2.1
    Atualizando classe game

--- src/main/java/com/dev/dslist/entities/Game.java
+++ src/main/java/com/dev/dslist/entities/Game.java
@@ -125,15 +125,5 @@ public class Game {
         return Objects.hash(id);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Game other = (Game) obj;
-        return Objects.equals(id, other.id);
-    }
+    
 }
