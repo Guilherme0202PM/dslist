fa75a58ee557ae86e3e820676ba941a21372ad74
Guilherme0202PM <46054442+Guilherme0202PM@users.noreply.github.com>
2025-01-17 09:10:55 -0300

    Envio 4.4 API Lists
    Atualizações no Projeto DSList
    
    As seguintes classes e alterações foram adicionadas ao projeto para implementar uma funcionalidade de gerenciamento de listas de jogos e permitir a interação via API REST no endpoint /lists. Classes e Funcionalidades Adicionadas:
    
    GameListService:
    Camada de serviço responsável por encapsular a lógica de negócio relacionada às listas de jogos.
    Adicionados métodos para:
    Buscar todas as listas de jogos (findAll).
    Buscar uma lista de jogos específica por ID (findById).
    Utiliza transações somente de leitura para melhorar a eficiência e a segurança das operações de consulta.
    
    GameListRepository:
    Repositório que herda de JpaRepository, fornecendo acesso direto ao banco de dados para a entidade GameList.
    Incluído método customizado updateBelongingPosition, que permite atualizar a posição de um jogo dentro de uma lista usando SQL nativo.
    
    GameListDTO:
    Classe DTO para transferir dados da entidade GameList de forma segura e eficiente.
    Facilita a exposição de dados apenas relevantes (ID e nome) para o cliente da API.
    
    GameListController:
    Controlador REST responsável por expor os endpoints para interação com as listas de jogos:
    GET /lists: Retorna todas as listas de jogos no formato JSON.
    GET /lists/{id}: Retorna uma lista específica de jogos por ID.
    Implementação do padrão MVC, separando claramente as responsabilidades.

---
+++ src/main/java/com/dev/dslist/controllers/GameListController.java
@@ -0,0 +1,40 @@
+package com.dev.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.dslist.dto.GameListDTO;
+import com.dev.dslist.dto.GameMinDTO;
+import com.dev.dslist.services.GameListService;
+import com.dev.dslist.services.GameService;
+
+@RestController 
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameListDTO findById(@PathVariable Long id) {
+        GameListDTO result = gameListService.findById(id);
+        return result;
+    }
+
+    @GetMapping
+    public List<GameListDTO> findAll() {
+        List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+
+}
---
+++ src/main/java/com/dev/dslist/services/GameListService.java
@@ -0,0 +1,36 @@
+package com.dev.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dev.dslist.dto.GameListDTO;
+import com.dev.dslist.entities.GameList;
+import com.dev.dslist.repositories.GameListRepository;
+import com.dev.dslist.repositories.GameRepository;
+
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    @Transactional(readOnly = true) 
+    public List<GameListDTO> findAll() {
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(GameListDTO::new).toList();
+    }
+
+
+    @Transactional(readOnly = true)
+    public GameListDTO findById(Long id) {
+        GameList entity = gameListRepository.findById(id).get();
+        return new GameListDTO(entity);
+    }
+}
